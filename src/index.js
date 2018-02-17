33b8789b8b224871948b8d99abf4fc448609a30b
Ivan Mattie <mattieivan@gmail.com>
2018-02-17 11:10:54 -0700

    Added docblocks to main file

--- src/index.js
+++ src/index.js
@@ -3,14 +3,38 @@ import * as ToastPriority from "./ToastPriority";
 
 var controller = new ToastManager();
 
+/**
+ * Opens a new Toast and returns the instance of it.
+ * 
+ * @param {Object} config
+ * @param {String} config.name The name of the new Toast. Can be used to
+ *  reference it later.
+ * @param {String} config.text The text to display within the toast.
+ * @param {String=} config.icon The name of the icon to display.
+ * @param {Number=} config.delay Defaults to 5000. Amount of time, in whole 
+ *  milliseconds, to wait before automatically hiding the Toast.
+ * @param {ToastPriority=} config.priority Determines the priority of the Toast,
+ *  which informs the screen reader appropriately.
+ */
 export var openToast = function(config) {
     return controller.openToast(config);
 };
 
+/**
+ * Closes a toast based on the name set when the Toast was opened.
+ * 
+ * @param {String} name The name of the toast set when the Toast was opened.
+ */
 export var closeToast = function(name) {
     controller.closeToast(name);
 };
 
+/**
+ * Opens a "sucess" style Toast. This Toast has a checkmark next to the text,
+ * low priority, and a slightly different style.
+ * 
+ * @param {String} text The text to display in the Toast.
+ */
 export var success = function(text) {
     return controller.openToast({
         name: "sucess",
@@ -20,6 +44,12 @@ export var success = function(text) {
     });
 };
 
+/**
+ * Opens an "error" style Toast. The Toast has an error symbol next to the text,
+ * high priority, and a slightly different style.
+ * 
+ * @param {String} text The text to display in the Toast.
+ */
 export var error = function(text) {
     return controller.openToast({
         name: "error",
@@ -30,6 +60,12 @@ export var error = function(text) {
     });
 };
 
+/**
+ * Opens a "warning" style Toast. The Toast has a warning symbol next to the 
+ * text, high priority, and a slightly different style.
+ * 
+ * @param {String} text The text to display in the Toast.
+ */
 export var warn = function(text) {
     return controller.openToast({
         name: "warning",
